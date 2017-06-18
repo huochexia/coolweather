2de99e2cb4e76f4c47a6cb79d6d00622205e2f79
Liuyong <huochex@sohu.com>
2017-06-18 14:49:30 +0800

    完成天气界面布局设计

--- app/src/main/java/com/learnandroid/liuyong/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/learnandroid/liuyong/coolweather/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.learnandroid.liuyong.coolweather;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -84,6 +85,12 @@ public class ChooseAreaFragment extends Fragment {
                 } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(position);
                     queryCities();
+                } else if (currentLevel == LEVEL_COUNTY) {
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(),WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
@@ -138,7 +145,7 @@ public class ChooseAreaFragment extends Fragment {
             currentLevel = LEVEL_CITY;
         } else {
             int provinceCode = selectedProvince.getProvinceCode();
-            String address ="http://guolin.tech/api/china"+provinceCode;
+            String address ="http://guolin.tech/api/china/"+provinceCode;
             queryFromServer(address,"city");
         }
     }
--- app/src/main/java/com/learnandroid/liuyong/coolweather/util/Utility.java
+++ app/src/main/java/com/learnandroid/liuyong/coolweather/util/Utility.java
@@ -2,9 +2,11 @@ package com.learnandroid.liuyong.coolweather.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.learnandroid.liuyong.coolweather.db.City;
 import com.learnandroid.liuyong.coolweather.db.County;
 import com.learnandroid.liuyong.coolweather.db.Province;
+import com.learnandroid.liuyong.coolweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -81,5 +83,20 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON 数据解析成Weather 实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
